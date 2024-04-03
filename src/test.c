edd056073d0fc097dc4d7ecc783e2acd48b2e7ce
Nedal Benelmekki <n.martinez@onalabs.com>
2024-04-03 12:40:43 +0200

    Minor changes and new approach attempt

--- src/test.c
+++ src/test.c
@@ -3,13 +3,41 @@
 #define MAX_VALUE 0xFFFFFFFFU
 #define MIN_VALUE 0x00000000U
 
-static inline uint32_t calc_mid(uint32_t left, uint32_t right)
+// Cuando buscamos el minimo, reemplazamos los 0x00000000 por 0xFFFFFFFF y buscamos el pivot point.
+// Cuando buscamos el maximo, reemplazamos los 0xFFFFFFFF por 0x00000000 y buscamos el pivot point.
+
+/**
+ * Calculates the midpoint between two given values.
+ * @author Albert Alvarez
+ * @param left: The left-most index in an array.
+ * @param right: The right-most index in an array.
+ * @return The midpoint between the left and right values.
+ */
+static inline uint32_t calculate_mid(uint32_t left, uint32_t right)
 {
-    return (left + (((right - left) >> 1)));
+    return left + (((right - left) >> 1));
 }
 
+/**
+ * Check if the conditions for initializing FIFO indexes are met.
+ * @author Nedal Martinez Benelmekki
+ * @param arr: Pointer to an array of uint32_t values.
+ * @param size: The size of the array.
+ * @param init_fifo_indexes_res: Pointer to a Fifo_Indexes_t structure to store the result.
+ * @return True if the conditions are met, False otherwise.
+ */
 bool check_conditions(uint32_t *arr, size_t size, Fifo_Indexes_t *init_fifo_indexes_res)
 {
+    if (arr == NULL)
+    {
+        return false;
+    }
+
+    if(init_fifo_indexes_res == NULL)
+    {
+        return false;
+    }
+
     if (size == 0)
     {
         return false;
@@ -17,7 +45,7 @@ bool check_conditions(uint32_t *arr, size_t size, Fifo_Indexes_t *init_fifo_inde
 
     if (size == 1)
     {
-        // For future use: Error handling/NVF_Status return value in function
+        // For future use
         /*
         if (arr[0] > MIN_VALUE && arr[0] < MAX_VALUE) {
             init_fifo_indexes_res->tail = 0;
@@ -29,63 +57,72 @@ bool check_conditions(uint32_t *arr, size_t size, Fifo_Indexes_t *init_fifo_inde
     return true;
 }
 
-void find_min_index(uint32_t *arr, size_t size, Fifo_Indexes_t *init_fifo_indexes_res)
+/**
+ * Replaces a value with a new value if it matches a specified old value.
+ * @authors Albert Alvarez, Nedal Martinez Benelmekki
+ * @param value: The value to be replaced.
+ * @param old_value: The value to be checked against.
+ * @param new_value: The value to replace with if the old value matches.
+ * @return The replaced value if it matches the old value, otherwise the original value.
+ */
+static inline uint32_t replace_value(uint32_t value, uint32_t old_value, uint32_t new_value)
 {
-    uint32_t left_idx = 0U;
-    uint32_t right_idx = size - 1U;
-    uint32_t mid_idx;
-
-    // Buscamos el minimimo
-    while (left_idx < right_idx)
-    {
-        // Reemplazamos los 0x00000000U por 0xFFFFFFFFU
-        uint32_t arr_mid_idx = (arr[mid_idx] == MIN_VALUE) ? MAX_VALUE : arr[mid_idx];
-        uint32_t arr_mid_p1_idx = (arr[mid_idx + 1U] == MIN_VALUE) ? MAX_VALUE : arr[mid_idx + 1U];
-        uint32_t arr_right_idx = (arr[right_idx] == MIN_VALUE) ? MAX_VALUE : arr[right_idx];
-
-        if (arr_mid_idx > arr_mid_p1_idx)
-        {
-            init_fifo_indexes_res->tail = mid_idx + 1U;
-            break;
-        }
-
-        if (mid_idx > MIN_VALUE)
-        {
-            uint32_t arr_mid_m1_idx = (arr[mid_idx - 1U] == MIN_VALUE) ? MAX_VALUE : arr[mid_idx - 1U];
-            if (arr_mid_m1_idx > arr_mid_idx)
-            {
-                init_fifo_indexes_res->tail = mid_idx;
-                break;
-            }
-        }
-
-        if (arr_mid_idx > arr_right_idx)
-        {
-            left_idx = mid_idx + 1U;
-        }
-        else
-        {
-            right_idx = mid_idx - 1U;
-        }
-
-        mid_idx = calc_mid(left_idx, right_idx);
-        init_fifo_indexes_res->tail = mid_idx;
-    }
+    return (value == old_value) ? new_value : value;
 }
 
-void find_max_index(uint32_t *arr, size_t size, Fifo_Indexes_t *init_fifo_indexes_res)
+/**
+ * Checks if the current value in mid is the head of the NVF. The head is the maximum value in the array (smaller than UINT32_MAX).
+ * @author Nedal Martinez Benelmekki
+ * @param left: The left-most index in an array.
+ * @param right: The right-most index in an array.
+ * @param mid: The midpoint between the left and right values.
+ * @return True if the conditions are met, False otherwise.
+ */
+static inline bool is_head(uint32_t *arr, uint32_t left, uint32_t right, uint32_t mid)
+{
+    return ((mid == 0U) && (arr[mid] >= arr[left]) && (arr[mid] >= arr[right]));
+}
+
+/**
+ * Checks if the current value in mid is the tail of the NVF. The tail is the minimum value in the array (larger than zero).
+ * @author Nedal Martinez Benelmekki
+ * @param left: The left-most index in an array.
+ * @param right: The right-most index in an array.
+ * @param mid: The midpoint between the left and right values.
+ * @return True if the conditions are met, False otherwise.
+ */
+static inline bool is_tail(uint32_t *arr, uint32_t left, uint32_t right, uint32_t mid)
+{
+    return ((mid == 0U) && (arr[mid] <= arr[left]) && (arr[mid] <= arr[right]));
+}
+
+/**
+ * Initializes the head index of the FIFO non volatile memory. It searches for the maximum value in the array and sets the head index accordingly.
+ * @author Albert Alvarez
+ * @param arr: Pointer to an array of uint32_t values.
+ * @param size: The size of the array.
+ * @param init_fifo_indexes_res: Pointer to a Fifo_Indexes_t structure to store the result.
+ * @return None.
+ */
+void init_fifo_head(uint32_t *arr, size_t size, Fifo_Indexes_t *init_fifo_indexes_res)
 {
     uint32_t left_idx = 0U;
     uint32_t right_idx = size - 1U;
-    uint32_t mid_idx;
+    uint32_t mid_idx = calculate_mid(left_idx, right_idx);
 
     // Buscamos el maximo
     while (left_idx < right_idx)
     {
+        if (is_head(arr, left_idx, right_idx, mid_idx))
+        {
+            init_fifo_indexes_res->head = mid_idx;
+            break;
+        }
+
         // Reemplazamos los 0xFFFFFFFFU por 0x00000000U
-        uint32_t arr_mid_idx = (arr[mid_idx] == MAX_VALUE) ? MIN_VALUE : arr[mid_idx];
-        uint32_t arr_mid_p1_idx = (arr[mid_idx + 1U] == MAX_VALUE) ? MIN_VALUE : arr[mid_idx + 1U];
-        uint32_t arr_right_idx = (arr[right_idx] == MAX_VALUE) ? MIN_VALUE : arr[right_idx];
+        uint32_t arr_mid_idx = replace_value(arr[mid_idx], MAX_VALUE, MIN_VALUE);
+        uint32_t arr_mid_p1_idx = replace_value(arr[mid_idx + 1U], MAX_VALUE, MIN_VALUE);
+        uint32_t arr_right_idx = replace_value(arr[right_idx], MAX_VALUE, MIN_VALUE);
 
         if (arr_mid_idx > arr_mid_p1_idx)
         {
@@ -95,7 +132,7 @@ void find_max_index(uint32_t *arr, size_t size, Fifo_Indexes_t *init_fifo_indexe
 
         if (mid_idx > 0U)
         {
-            uint32_t arr_mid_m1_idx = (arr[mid_idx - 1U] == MAX_VALUE) ? MIN_VALUE : arr[mid_idx - 1U];
+            uint32_t arr_mid_m1_idx = replace_value(arr[mid_idx - 1U], MAX_VALUE, MIN_VALUE);
             if (arr_mid_m1_idx > arr_mid_idx)
             {
                 init_fifo_indexes_res->head = mid_idx;
@@ -112,8 +149,69 @@ void find_max_index(uint32_t *arr, size_t size, Fifo_Indexes_t *init_fifo_indexe
             right_idx = mid_idx - 1U;
         }
 
-        mid_idx = calc_mid(left_idx, right_idx);
+        mid_idx = calculate_mid(left_idx, right_idx);
         init_fifo_indexes_res->head = mid_idx;
+    }
+}
+
+/**
+ * Initializes the tail index of the FIFO non volatile memory. It searches for the minimum value in the array and sets the tail index accordingly.
+ * @author Albert Alvarez
+ * @param arr: Pointer to an array of uint32_t values.
+ * @param size: The size of the array.
+ * @param init_fifo_indexes_res: Pointer to a Fifo_Indexes_t structure to store the result.
+ * @return None.
+ */
+void init_fifo_tail(uint32_t *arr, size_t size, Fifo_Indexes_t *init_fifo_indexes_res)
+{
+    uint32_t left_idx = 0U;
+    uint32_t right_idx = size - 1U;
+    uint32_t mid_idx = calculate_mid(left_idx, right_idx);
+
+    // Buscamos el minimimo
+    while (left_idx < right_idx)
+    {
+        if (is_tail(arr, left_idx, right_idx, mid_idx))
+        {
+            init_fifo_indexes_res->tail = mid_idx;
+            break;
+        }
+
+        // Reemplazamos los 0x00000000U por 0xFFFFFFFFU
+        uint32_t arr_mid_idx = replace_value(arr[mid_idx], MIN_VALUE, MAX_VALUE);
+        uint32_t arr_mid_p1_idx = replace_value(arr[mid_idx + 1U], MIN_VALUE, MAX_VALUE);
+        uint32_t arr_right_idx = replace_value(arr[right_idx], MIN_VALUE, MAX_VALUE);
+
+        if (arr_mid_idx > arr_mid_p1_idx)
+        {
+            init_fifo_indexes_res->tail = mid_idx + 1U;
+            break;
+        }
+
+        if (mid_idx > 0U)
+        {
+            uint32_t arr_mid_m1_idx = replace_value(arr[mid_idx - 1U], MIN_VALUE, MAX_VALUE);
+            if (arr_mid_m1_idx > arr_mid_idx)
+            {
+                init_fifo_indexes_res->tail = mid_idx;
+                break;
+            }
+        }
+
+        if (arr_mid_idx > arr_right_idx)
+        {
+            left_idx = mid_idx + 1U;
+        }
+
+        // In case of Right == Left == Mid == Mid+1 == Mid-1 --> What half do we search? Linear search for a worse case scenario?
+
+        else
+        {
+            right_idx = mid_idx - 1U;
+        }
+
+        mid_idx = calculate_mid(left_idx, right_idx);
+        init_fifo_indexes_res->tail = mid_idx;
     }
 }
 
@@ -126,10 +224,27 @@ Fifo_Indexes_t init_fifo_indexes(uint32_t *arr, size_t size)
         return init_fifo_indexes_res;
     }
 
-    find_min_index(arr, size, &init_fifo_indexes_res);
-
-    find_max_index(arr, size, &init_fifo_indexes_res);
+    init_fifo_head(arr, size, &init_fifo_indexes_res);
+    init_fifo_tail(arr, size, &init_fifo_indexes_res);
 
     return init_fifo_indexes_res;
 }
 
+int NVF_init(Fifo_Indexes_t *self, uint32_t *arr, size_t size)
+{
+    if (self == NULL)
+    {
+        return -1;
+    }
+    self->head = 0x0U;
+    self->tail = 0x0U;
+
+    Fifo_Indexes_t x = init_fifo_indexes(arr, size);
+
+    if (x.head == 0U && x.tail == 0U)
+    {
+        return -1;
+    }
+
+    return 0;
+}
